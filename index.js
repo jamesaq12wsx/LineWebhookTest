d91646cc4e1ede48a68f083bfedf41ea81965d12
ShaoChin Lin <jamesaq12wsx@gmail.com>
2018-08-11 11:18:50 +0800

    use linebot package

--- index.js
+++ index.js
@@ -1,39 +1,22 @@
-const line = require('@line/bot-sdk');
-const express = require('express');
-const lineConfig = {
-    channelAccessToken: 'ovbpoqGP/Fy5is/rGcT6BtboGtvrGakyP1Rkslse3IuCRvnUZ4MMEVIjBhfsyJZeC23Iis6KapcdLrMIug/jPEGa7SjU+fzJ8KKc1VrE1fSLKk9YxS27A5UqM8pyz9TH08s3Y8jKjRweZw8LV9BqnQdB04t89/1O/w1cDnyilFU=',
-    channelSecret: 'fb2f25eda69ebc51cf2dc5a4d2c9d174'
-};
-const client = new line.Client(lineConfig);
-const app = express();
-
-app.listen(8080, function () {
-    console.log('App now running on port', this.address().port);
+let linebot = require('linebot'),
+    express = require('express');
+const config = require('./config.json'),
+    util = require('util');
+let bot = linebot({
+    channelId: '1599437019',
+    channelSecret: 'fb2f25eda69ebc51cf2dc5a4d2c9d174',
+    channelAccessToken: 'ovbpoqGP/Fy5is/rGcT6BtboGtvrGakyP1Rkslse3IuCRvnUZ4MMEVIjBhfsyJZeC23Iis6KapcdLrMIug/jPEGa7SjU+fzJ8KKc1VrE1fSLKk9YxS27A5UqM8pyz9TH08s3Y8jKjRweZw8LV9BqnQdB04t89/1O/w1cDnyilFU='
 });
-
-app.post('/', line.middleware(lineConfig), function (req, res) {
-    Promise
-        .all(req.body.events.map(handleEvent))
-        .then(function (result) {
-            res.json(result);
-        });
+const linebotParser = bot.parser(),
+    app = express();
+    
+bot.on('message', function (event) {
+    // 把收到訊息的 event 印出來
+    console.log(event);
 });
-
-function handleEvent(event) {
-    switch (event.type) {
-        case 'join':
-        case 'follow':
-            return client.replyMessage(event.replyToken, {
-                type: 'text',
-                text: '你好請問我們認識嗎?'
-            });
-        case 'message':
-            switch (event.message.type) {
-                case 'text':
-                    return client.replyMessage(event.replyToken, {
-                        type: 'text',
-                        text: (event.message.text + '~*')
-                    });
-            }
-    }
-}
+app.post('/webhook', linebotParser);
+// 在 localhost 走 8080 port
+let server = app.listen(process.env.PORT || 8080, function () {
+    let port = server.address().port;
+    console.log("My Line bot App running on port", port);
+});
